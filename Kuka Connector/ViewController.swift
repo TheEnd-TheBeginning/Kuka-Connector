0bc575936f50e41ac1cf4caf413875c5708d0532
Alex <asavicheva@internet.ru>
2023-01-10 13:58:09 +0300

    Initial Commit

---
+++ Kuka Connector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kuka Connector
+//
+//  Created by Пользователь on 10.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
