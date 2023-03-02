e586824b451a087f21de15be8fcbb30765fcd65f
alex_s <alexssurnames@gmail.com>
2023-03-02 14:16:19 +0300

    Make text font  "Menlo" for UnderwrittenTextField

--- Kuka Connector/View/UnderwrittenTextField.swift
+++ Kuka Connector/View/UnderwrittenTextField.swift
@@ -15,9 +15,10 @@ class UnderwrittenTextField: UITextField {
         self.textColor = UIColor.black.withAlphaComponent(0.5)
         self.layer.borderColor = UIColor.black.cgColor.copy(alpha: 0.2)
         self.layer.borderWidth = 3
-        self.font = UIFont(name: "Monle", size: 17)
         self.layer.cornerRadius = 5
 //        self.clipsToBounds = false
         self.borderStyle = .roundedRect
+//        self.defaultTextAttributes = [NSAttributedString.Key.font: UIFont(name: "Menlo", size: 17)]
+        self.font = UIFont(name: "Menlo", size: 17)
     }
 }
