1f50c77b16296313a78963c9ac431c5afc265e6d
Alex <asavicheva@internet.ru>
2023-03-06 03:04:15 +0300

    Reorganized some files

--- Kuka Connector/Model/Libs/Openshowvar.swift
+++ Kuka Connector/Model/Libs/Openshowvar.swift
@@ -12,31 +12,20 @@ class Openshowvar {
     var port: UInt16?
     var msg_id: Int = Int.random(in: 1...100)
     var sock: BSD_Socket?
-//    var can_connect: Box<Bool> = Box(false)
-//    init() {
-//        self.ip = "127.0.0.1"
-//        self.port = 0
-//        self.msg_id = Int.random(in: 1...100)
-//        self.sock = BSD_Socket(Domain: AF_INET, Type: SOCK_STREAM, Protocol: IPPROTO_TCP)
-//        print("Void init")
-//    }
+    
     func connect(ip: String, port: UInt16) -> Bool {
         self.ip = ip
         self.port = port
-//        self.msg_id = Int.random(in: 1...100)
         self.sock = BSD_Socket(Domain: AF_INET, Type: SOCK_STREAM, Protocol: IPPROTO_TCP)
         if self.sock?.sock == -1 {
             print("Error creating BSD Socket")
-//            self.can_connect.value = false
             return false
         } else {
             let sockConnect = self.sock?.connectSocket(ip: self.ip!, port: self.port!)
             if (sockConnect == -1) {
                 print("Error connecting BSD Socket")
-//                self.can_connect.value = false
                 return false
             } else {
-//                self.can_connect.value = true
                 return true
             }
         }
@@ -137,74 +126,4 @@ class Openshowvar {
         let recvSuccess = recv(self.sock!.sock, &recvBuffer, 256, 0)
         return recvBuffer
     }
-    
-//    def read(self, var, debug=True):
-//        if not isinstance(var, str):
-//            raise Exception('Var name is a string'
-//        else:
-//            self.varname = var if PY2 else var.encode(ENCODING)
-//        return self._read_var(debug)
-            
-//    def write(self, var, value, debug=True):
-//        if not (isinstance(var, str) and isinstance(value, str)):
-//            raise Exception('Var name and its value should be string')
-//        self.varname = var if PY2 else var.encode(ENCODING)
-//        self.value = value if PY2 else value.encode(ENCODING)
-//        return self._write_var(debug)
-        
-//    def _pack_read_req(self):
-//        var_name_len = len(self.varname)
-//        flag = 0
-//        req_len = var_name_len + 3
-//        return struct.pack(
-//            '!HHBH'+str(var_name_len)+'s',
-//            self.msg_id,
-//            req_len,
-//            flag,
-//            var_name_len,
-//            self.varname
-//            )
-
-//    def _pack_write_req(self):
-//        var_name_len = len(self.varname)
-//        flag = 1
-//        value_len = len(self.value)
-//        req_len = var_name_len + 3 + 2 + value_len
-//        return struct.pack(
-//            '!HHBH'+str(var_name_len)+'s'+'H'+str(value_len)+'s',
-//            self.msg_id,
-//            req_len,
-//            flag,
-//            var_name_len,
-//            self.varname,
-//            value_len,
-//            self.value
-//            )
-    
-//    def _read_var(self, debug):
-//        req = self._pack_read_req()
-//        self._send_req(req)
-//        _value = self._read_rsp(debug)
-//        if debug:
-//            print(_value)
-//        return _value
-            
-//    def _write_var(self, debug):
-//        req = self._pack_write_req()
-//        self._send_req(req)
-//        _value = self._read_rsp(debug)
-//        if debug:
-//            print(_value)
-//        return _value
-    
-//    def _read_rsp(self, debug=False):
-//        if self.rsp is None: return None
-//        var_value_len = len(self.rsp) - struct.calcsize('!HHBH') - 3
-//        result = struct.unpack('!HHBH'+str(var_value_len)+'s'+'3s', self.rsp)
-//        _msg_id, body_len, flag, var_value_len, var_value, isok = result
-//        if debug:
-//            print('[DEBUG]', result)
-//        if result[-1].endswith(b'\x01') and _msg_id == self.msg_id:
-//            self.msg_id = (self.msg_id + 1) % 65536  # format char 'H' is 2 bytes long
-//            return var_value
 }
